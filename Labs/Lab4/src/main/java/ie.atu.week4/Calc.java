3e2d40264e6afb87242e8baa6db33eda84b0b20d
Corey4711 <G00381203@atu.ie>
2022-10-12 09:52:25 +0100

    Add Method

--- Labs/Lab4/src/main/java/ie.atu.week4/Calc.java
+++ Labs/Lab4/src/main/java/ie.atu.week4/Calc.java
@@ -1,17 +1,30 @@
 package ie.atu.week4;
 
+import java.util.Scanner;
+
 public class Calc {
 
-    static void main(String[] args)
+    public static void main(String[] args)
         {
+            int x, y, sum;
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the first number: ");
+            x = sc.nextInt();
+            System.out.print("Enter the second number: ");
+            y = sc.nextInt();
 
-
-            //ADD Corey
-
+            sum = add(x,y);
 
             //Multiply Lorcan
 
 
             //Delete Niall
         }
+
+    public static int add(int a, int b)
+    {
+        int sum = a + b;
+        System.out.print(a + " + " + b + " = " + sum);
+        return sum;
+    }
 }
