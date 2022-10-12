82a9efef73f771eca85a532f337646ddde17d85e
LorcanS1993 <G00289865@atu.ie>
2022-10-12 09:41:35 +0100

    Added new method Multiply

--- Labs/Lab4/src/main/java/ie.atu.week4/Calc.java
+++ Labs/Lab4/src/main/java/ie.atu.week4/Calc.java
@@ -10,7 +10,18 @@ public class Calc {
 
 
             //Multiply Lorcan
+public static void Multiply() {
+    System.out.println("Input your first number");
+    Scanner inputs = new Scanner(System.in);
+    int firstNumber = inputs.nextInt();
 
+    System.out.println("Input your second number");
+    int secondNumber = inputs.nextInt();
+
+    int total = firstNumber * secondNumber;
+    System.out.println("The total number is " + total);
+
+        }
 
             //Delete Niall
         }
