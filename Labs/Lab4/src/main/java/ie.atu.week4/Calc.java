e61be3fc82702c2e3532b1b5a9998ffcfd86be3f
G00362680 <G00362680@atu.ie>
2022-10-12 10:26:12 +0100

    subtraction module

--- Labs/Lab4/src/main/java/ie.atu.week4/Calc.java
+++ Labs/Lab4/src/main/java/ie.atu.week4/Calc.java
@@ -1,10 +1,12 @@
 package ie.atu.week4;
 
+import java.util.Scanner;
+
 public class Calc {
 
-    static void main(String[] args)
-        {
-
+    public static void main(String[] args)
+            {
+            int sum, firstinput,secondinput;
 
             //ADD Corey
 
@@ -13,5 +15,21 @@ public class Calc {
 
 
             //Delete Niall
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Please enter two numbers");
+            firstinput = sc.nextInt();
+            secondinput = sc.nextInt();
+            sum = delete(firstinput, secondinput);
+            System.out.println(sum);
+
+
+
         }
+    public static int delete(int x, int y) {
+        int sum;
+        sum = x-y;
+        return sum;
+    }
 }
